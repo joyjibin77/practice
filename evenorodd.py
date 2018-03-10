a1b4079d1ae77a038df95b4939c3649aeaabf5fa
joyjibin77 <37235014+joyjibin77@users.noreply.github.com>
2018-03-10 03:33:36 -0500

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+x=int(input("Enter a number: "))
+mod=x % 2
+if mod>0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
