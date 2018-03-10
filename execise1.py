3ccf25a825f6613dece7a8753f75adf2191bd71f
joyjibin77 <37235014+joyjibin77@users.noreply.github.com>
2018-03-10 01:42:52 -0500

    Add files via upload

---
+++ execise1.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     09/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+ # indented four spaces
+
+x=int(input("please enter a number"))
+
+if x<=10:
+    print("negative")
+elif x==0:
+    print("zero")
+elif x>100000:
+    print("enter correct value")
+else:
+    print("positive")
+
+
+
